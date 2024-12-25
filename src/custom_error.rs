0f10c6342a0c31c4714fee186aedff44e7a595bc
talchow <talchow.chow6@gmail.com>
2024-12-25 23:01:34 +0800

    import thiserror

--- src/custom_error.rs
+++ src/custom_error.rs
@@ -3,31 +3,24 @@ use std::error::Error;
 use std::str::FromStr;
 use std::string::FromUtf8Error;
 use std::sync::PoisonError;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug,Error)]
 pub enum ProcessError {
-    FromUtf8Error(FromUtf8Error),
+    #[error("not utf8 error")]
+    FromUtf8Error(#[from] FromUtf8Error),
+    #[error("poison error {0}")]
     PoisonError(String),
+    #[error("incomplete")]
     Incomplete,
 }
 
-impl fmt::Display for ProcessError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-       match self {
-           Self::FromUtf8Error(u) => write!(f,"{}",u),
-           Self::PoisonError(p) => write!(f, "{}",p),
-           Self::Incomplete => "the stream closed too early".fmt(f),
-       }
-    }
-}
 
-impl Error for ProcessError {}
-
-impl From<std::string::FromUtf8Error> for ProcessError {
-    fn from(value: FromUtf8Error) -> Self {
-        Self::FromUtf8Error(value)
-    }
-}    
+// impl From<std::string::FromUtf8Error> for ProcessError {
+//     fn from(value: FromUtf8Error) -> Self {
+//         Self::FromUtf8Error(value)
+//     }
+// }    
 
 // impl From<std::sync::PoisonError<String>> for ProcessError {
 //     fn from(value: PoisonError<String>) -> Self {
