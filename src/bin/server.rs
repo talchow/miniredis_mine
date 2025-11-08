e95c220b38a2bf413a335b7017a0c638eb4dd82f
talchow <talchow.chow6@gmail.com>
2025-11-09 01:31:16 +0800

    import axum & http

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,90 +1,117 @@
-use core::str;
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use axum::http::Response;
+use axum::Router;
+use axum::routing::post;
+use tokio::sync::Mutex;
 
-use bytes::{Buf, Bytes, BytesMut};
-use cli_for_miniredis::ProcessError;
+use bytes::Bytes;
 use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener;
 
+use cli_for_miniredis::execute_data::execute_cmd;
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+   let db = Arc::new(Mutex::new(HashMap::new()));
 
-    println!("Listening!");
+   let listener = TcpListener::bind("127.0.0.1:6379").await?;
+   println!("TCP listening on 127.0.0.1:6379");
+   tokio::spawn(tcp_listener(listener,db.clone()));
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
+   println!("HTTP api listening on 127.0.0.1:8080");
+   let app = Router::new()
+       .route("/api/redis", post(move |body:String| 
+    redis_handler(body,db.clone())));
+    let addr = SocketAddr::from(([127,0,0,1],8080));
+    println!("HTTP listening on http://{addr}/api/redis");
 
+    tokio::spawn(async move {
+        let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+        axum::serve(listener, app).await.unwrap();
+    }); 
+
+    tokio::signal::ctrl_c().await?;
+    println!("Shutting down");
+    Ok(())
+}
+
+async fn tcp_listener(listener: TcpListener, db: Db) {
     loop {
-        let (socket, _) = listener.accept().await?;
-
-        let db: Db = db.clone();
-
-        println!("Accepted!");
-
+        let (mut socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
         tokio::spawn(async move {
-            if let Err(err) = process(socket, db).await {
-                eprintln!("processor error: {err}");
-                // match err {
-                //   ProcessError::FromUtf8Error()  > x 
-                //   ProcessError::FromUtf8Error()  > x 
-                // }
+            let mut buf = vec![0u8;1024];
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        let input = String::from_utf8_lossy(&buf[..n]);
+                        let rsp = execute_cmd(&input, db.clone()).await;
+
+                        if socket.write_all(rsp.as_bytes()).await.is_err() {break;}
+                    }
+                    Err(_) => break,
+                }
             }
         });
     }
 }
 
-async fn process(mut socket: TcpStream, db: Db) -> Result<(), ProcessError> {
-    // type Result<T> = core::result::Result<T, ProcessError>;
-    // 创建一个缓冲区，用于存储从客户端读取的数据
-    // 把socket数据读到buf中
-    let mut buf = BytesMut::new();
-    let _ = socket.read_buf(&mut buf).await;
-    println!("reach here");
-    // 循环处理buf中的数据,如果数据开头是*，则认为是一个完整的frame,写入frame中，否则认为是一个不完整的frame
-    loop {
-        let frame: Vec<u8> = match buf.get_u8() {
-            b'*' => buf.to_vec(),
-            _ => break Ok(()),
-        };
-        // 将frame转换为字符串,并根据"\r\n"分割为vec
-        let frame_string = String::from_utf8(frame)?;
-        let vec_data: Vec<&str> = frame_string.split("\r\n").collect();
-
-        let res = match vec_data[0] {
-            "2" => {
-                let db = match db.lock() {
-                    Ok(db) => db,
-                    Err(_e) => return Err(ProcessError::PoisonError("DB lock".to_string())),
-                };
-
-                if let Some(value) = db.get(vec_data[4]) {
-                    value.clone()
-                } else {
-                    break Ok(());
-                }
-            }
-            "3" => {
-                let mut db = db
-                    .lock()
-                    .map_err(|_| ProcessError::PoisonError("DB lock".to_string()))?;
-                db.insert(vec_data[4].to_string(), vec_data[6].to_owned().into());
-                "ok".into()
-            }
-            _ => {
-                println!("frame format is error!");
-                break Ok(());
-            }
-        };
-        let _ = match socket.write_all(&res).await {
-            Ok(_) => {
-                println!("writed!");
-            }
-            Err(_e) => return Err(ProcessError::Incomplete),
-        };
-    }
+async fn redis_handler(body: String, db: Db) -> Response<String> {
+    let rsp = execute_cmd(&body, db).await;
+    Response::builder()
+        .header("Access-Control-Allow-Origin", "*")
+        .body(rsp)
+        .unwrap()
 }
+
+
+
+// async fn process(mut socket: TcpStream, db: Db) -> Result<(), ProcessError> {
+//     // type Result<T> = core::result::Result<T, ProcessError>;
+//     // 创建一个缓冲区，用于存储从客户端读取的数据
+//     // 把socket数据读到buf中
+//     let mut buf = BytesMut::new();
+//     let _ = socket.read_buf(&mut buf).await;
+//     println!("reach here");
+//     // 循环处理buf中的数据,如果数据开头是*，则认为是一个完整的frame,写入frame中，否则认为是一个不完整的frame
+//     loop {
+//         let frame: Vec<u8> = match buf.get_u8() {
+//             b'*' => buf.to_vec(),
+//             _ => break Ok(()),
+//         };
+//         // 将frame转换为字符串,并根据"\r\n"分割为vec
+//         let frame_string = String::from_utf8(frame)?;
+//         let vec_data: Vec<&str> = frame_string.split("\r\n").collect();
+
+//         let res = match vec_data[0] {
+//             "2" => {
+//                 let db_guard = db.lock().await;
+
+//                 if let Some(value) = db_guard.get(vec_data[4]) {
+//                     value.clone()
+//                 } else {
+//                     break Ok(());
+//                 }
+//             }
+//             "3" => {
+//                 let mut db_guard = db.lock().await;
+//                 db_guard.insert(vec_data[4].to_string(), vec_data[6].to_owned().into());
+//                 "ok".into()
+//             }
+//             _ => {
+//                 println!("frame format is error!");
+//                 break Ok(());
+//             }
+//         };
+//         let _ = match socket.write_all(&res).await {
+//             Ok(_) => {
+//                 println!("writed!");
+//             }
+//             Err(_e) => return Err(ProcessError::Incomplete),
+//         };
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ pub use set::do_set;
 
 pub mod custom_error;
 pub use custom_error::ProcessError;
+
+pub mod execute_data;
+pub use execute_data::execute_cmd;
